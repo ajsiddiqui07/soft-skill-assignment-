c8fbab8da5f897c3986f231165f856ca6690c4d3
arbazsiddiqui <arbazsiddiqui769@gmail.com>
2025-11-16 16:21:58 +0530

    calculater

---
+++ PRACTICE/calculater.py
@@ -0,0 +1,32 @@
+again ='yes'
+while again =='yes'  :
+    value_1 = int(input("Enter first value : "))
+    value_2 = int(input("Enter second value : "))
+    print("""1. enter 1 for sum
+            2. enter 2 for sub
+            3. enter 3 for mult
+            4. enter 4 for div """)
+    choice=int(input("choose 1 to 4 num for perform specific task : "))
+    if choice==1:
+        print(value_1+value_2)
+    elif choice==2:
+        print(value_1-value_2)
+    elif choice==3:
+        print(value_1*value_2)
+    elif choice==4:
+        print(value_1/value_2)
+    while again!='yes' or again!='no':
+        again=input("do you want to con? yes or no : ")
+        if
+        print("please enter no if you not want to continue")
+        if again=='yes' or again=='no':
+            break 
+    # again=input("do you want to con? y or n : ")
+    # if again =='y'or again == 'N':
+       
+    #     continue
+    # else:
+    #     while again!='y' or again!='n':
+    #         print("not valid")
+            
+        
